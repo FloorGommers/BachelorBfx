008a012de946c4ce10b2c843b3e2e199e913b9ff
FloorGommers <39589631+FloorGommers@users.noreply.github.com>
2018-05-24 14:38:24 +0200

    Update WES_analysis_pipeline.sh

--- WES_analysis_pipeline.sh
+++ WES_analysis_pipeline.sh
@@ -1,23 +1,23 @@
 # WES ANALYSIS
 # --------------------------------
 # Map to reference genome
-bwa mem Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta datafiles/na12878_wes_brcagenes-1.fastq datafiles/na12878_wes_brcagenes-2.fastq > na12878_wes.sam
+bwa mem home/ubuntu/Notebooks/Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta datafiles/na12878_wes_brcagenes-1.fastq datafiles/na12878_wes_brcagenes-2.fastq > na12878_wes.sam
 
 # Create BAM file
 sambamba view -S -f bam na12878_wes.sam > na12878_wes.bam
 
-# Mark duplicates
-sambamba markdupe na12878_wgs.sorted.bam dedupped_na12878_wes.sorted.bam
-
 # Sort the BAM file
 sambamba sort na12878_wes.bam
+
+# Mark duplicates
+sambamba markdup na12878_wes.sorted.bam dedupped_na12878_wes.sorted.bam
 
 # Add readgroups
 java -jar $PICARD AddOrReplaceReadGroups \
 I=$inputfile dedupped_na12878_wes.sorted.bam \
 O=$outputfile RG_dedupped_na12878_wes.sorted.bam \
 CREATE_INDEX=true \
-RGLB=$LB WES \
+RGLB=$LB wes \
 RGPL=$PL illumina \
 RGSM=$SM na12878 \
 RGPU=$PU slide_barcode
@@ -26,7 +26,7 @@ RGPU=$PU slide_barcode
 java -jar $GATK \
 -T HaplotypeCaller \
 -R Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta  \
--I  RG_dedupped_na12878_wes.sorted.bam \
+-I RG_dedupped_na12878_wes.sorted.bam \
 -o RG_dedupped_na12878_wes.sorted.vcf
 
 # --------------------------------
